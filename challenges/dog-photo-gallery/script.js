a51872f544c216324862db07bee124d29023fa2d
Sarah Abu Zeneh <sara.abu.zena2000@gmail.com>
2021-08-18 20:01:24 +0300

    Compleate dog-photo-gallery Challenge

---
+++ challenges/dog-photo-gallery/script.js
@@ -0,0 +1,16 @@
+let clickedButton = document.getElementById("clicked-button");
+let imageLi = document.querySelector(".image-container");
+let url = `https://dog.ceo/api/breeds/image/random`;
+let dogImage = document.createElement('img');
+ imageLi.appendChild(dogImage);
+ imageLi.style.display='none';
+clickedButton.addEventListener('click',function(){
+    fetch(url)
+    .then(response=>{
+        return response.json();
+    }).then(data=>{
+        imageLi.style.display='block';
+        console.log(data.message);
+        dogImage.src=data.message;
+        })
+});
